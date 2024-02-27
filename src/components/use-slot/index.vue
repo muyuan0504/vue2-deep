ba00b4fd20e8345bd53c7753b1111a5f8f416d8d
aiden <972523881@qq.com>
2024-02-27 17:25:23 +0800

    fix: slot

--- src/components/use-slot/index.vue
+++ src/components/use-slot/index.vue
@@ -17,7 +17,11 @@ export default {
     },
     computed: {},
     created() {
-        /** slot的节点内容，会挂载到组件实例的$slots上 */
+        /** 
+         * slot的节点内容，会挂载到组件实例的$slots上
+         * 
+         * 在使用作用域插槽时，父组件传递给子组件的内容是通过作用域插槽的方式传递的，而不是通过 $slots 属性。因此，如果在子组件中直接打印 this.$slots 是无法获取作用域插槽内容的
+         */
         console.log('use-slot', this.$slots)
     },
     methods: {},
